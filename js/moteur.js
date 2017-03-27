5c18479c69e81f55a8b148f75d24b6b9a7e7a6e4
castano cindy <castanocindy@castanos-MacBook-Pro.local>
2017-03-27 22:14:16 +0200

    responsive 768

--- js/moteur.js
+++ js/moteur.js
@@ -36,13 +36,14 @@ PARALAX
     
     $(function () {
         $(window).scroll(function () {
-            $('#about-bio').css({'top': function (index, value) {return 150 - $(window).scrollTop() * 0.4}});
-            $('#about-bio2').css({'top': function (index, value) {return 400 - $(window).scrollTop() * 0.3}});
-            $('#span-bio').css({'top': function (index, value) {return 400 - $(window).scrollTop() * 0.3}});
-            $('#team-h3').css({'top': function (index, value) {return 830 - $(window).scrollTop() * 0.5}});
-            $('#h3-span').css({'top': function (index, value) {return 820 - $(window).scrollTop() * 0.5}});
-            $('#team').css({'top': function (index, value) {return 870 - $(window).scrollTop() * 0.4}});
-            $('h4').css({'top': function (index, value) {return 1700 - $(window).scrollTop() * 0.2}});
+            $('#title-h3').css({'top': function (index, value) {return 150 - $(window).scrollTop() * 0.2}});
+            $('#about-bio').css({'top': function (index, value) {return 300 - $(window).scrollTop() * 0.4}});
+            $('#about-bio2').css({'top': function (index, value) {return 550 - $(window).scrollTop() * 0.5}});
+            $('#span-bio').css({'top': function (index, value) {return 550 - $(window).scrollTop() * 0.5}});
+            $('#team-h3').css({'top': function (index, value) {return 900 - $(window).scrollTop() * 0.4}});
+            $('#h3-span').css({'top': function (index, value) {return 880 - $(window).scrollTop() * 0.4}});
+            $('#team').css({'top': function (index, value) {return 950 - $(window).scrollTop() * 0.3}});
+            $('h4').css({'top': function (index, value) {return 2000 - $(window).scrollTop() * 0.2}});
     
         });
    
